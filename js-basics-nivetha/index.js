c2629dd50f7cbd5c896b086f498dce6df43536b3
Nivetha Rathnavel <nivetharath@gmail.com>
2021-03-08 14:23:49 +0530

    Add files via upload

---
+++ js-basics-nivetha/index.js
@@ -0,0 +1,56 @@
+//x+y is an expression,produces the value
+// x and y are operands and + is a operator
+// let x = 10;
+// //Increment (++)
+// console.log(++x); //increments and displays
+// console.log(x++); //displays and increments
+// //Decrement (--)
+// console.log(--x); //decrements and displays
+// console.log(x--); //displays and decrements
+
+// //Increment by 5
+// console.log(x+=5);
+
+
+// console.log(true === 1); //false
+// console.log(true == 1); //true
+
+//ternary operator 
+// let points = 100;
+// let typeOfCustomer = points > 100 ? 'gold' : 'silver';
+// console.log(typeOfCustomer);
+
+// let highIncome = true;
+// let goodCreditScore = true; 
+// let eligibleForLoan = highIncome && goodCreditScore;
+// console.log('Eligible for Loan', eligibleForLoan);
+
+// let x = 11;
+// console.log(true && ++x);
+// console.log(x);
+
+// console.log(false || -1 || 2); //1 //short-circuiting
+// console.log(true || 1 || 2); //true //short-circuiting
+
+
+// console.log(true && 1 && 2); //1 //short-circuiting
+
+// let d = NaN;
+// console.log(d && true);
+
+// let t = {};
+// console.log(t+[]);
+
+// console.log(0 | 1); //Bitwise OR
+// console.log(0 & 1); //Bitwise AND
+
+// console.log(2 * 8 / 2);
+// console.log(2 / 8 * 2);
+
+let a = 'red';
+let b = 'blue';
+
+[a, b] = [b, a];
+console.log(a, b);
+
+
